aee67850ae55a852bce136433e0e586d8d0ec662
Louis Kristian Goendong <luisluis3004@gmail.com>
2025-01-15 10:36:50 +0700

    Perubahan

--- Tiket.cpp
+++ Tiket.cpp
@@ -1,118 +1,101 @@
 #include <iostream>
 using namespace std;
 
-void pembelian_tiket_bioskop ()
- {
- int pilihan,angka;
+// Fungsi utama untuk pembelian tiket bioskop
+void pembelian_tiket_bioskop() {
+    int pilihan;
+    int totalKursi = 50; // Jumlah kursi total di bioskop
 
-  do
- {
-     cout << "\t   Bioskop XXI Bandung"; cout << endl;
-     cout << "\t JL.Bhayangkara Bandung"; cout << endl;
-     cout << " =============================================" << endl;
-     cout << "\t      DAFTAR MENU "; cout << endl;
-     cout << "\t---------------------------------" << endl;
-     cout << "\t| 1. Pembelian Tiket            |" << endl;
-     cout << "\t| 2. Cek Sisa Kursi             |" << endl;
-     cout << "\t| 3. Keluar \t		|" << endl;
-     cout << "\t---------------------------------" << endl;
-     cout << " =============================================" << endl;
-     cout << endl;
- 
-     cout << " Masukan Pilihan Anda (1-3) : "; cin >> pilihan;
-     cout << endl;
-     
-     system ("cls");
-     
-     switch (pilihan)
+    do {
+        // Tampilan menu utama
+        cout << "\t   Bioskop XXI Bandung" << endl;
+        cout << "\t JL.Bhayangkara Bandung" << endl;
+        cout << " =============================================" << endl;
+        cout << "\t      DAFTAR MENU " << endl;
+        cout << "\t---------------------------------" << endl;
+        cout << "\t| 1. Pembelian Tiket            |" << endl;
+        cout << "\t| 2. Cek Sisa Kursi             |" << endl;
+        cout << "\t| 3. Keluar                     |" << endl;
+        cout << "\t---------------------------------" << endl;
+        cout << " =============================================" << endl;
+        cout << endl;
 
-   {
+        cout << " Masukan Pilihan Anda (1-3) : ";
+        cin >> pilihan;
+        cout << endl;
 
-    case 1:
+        system("cls");
 
-     mulai:
-     int jenisTKT,jumlahTKT,totalTKT;
-     char* jenisTXT;
-     char ulang,belanjaKmbln;
+        switch (pilihan) {
+        case 1: {
+            // Pembelian tiket
+            int jenisTKT, jumlahTKT, totalTKT;
+            string jenisTXT;
 
-       cout << " =============================================" << endl;
-       cout << "\t   Pembelian Tiket "; cout << endl;
-       cout << " =============================================" << endl;     
-       cout << " Keterangan" << endl;
-       cout << " 1. Tiket Dewasa  : Rp. 35.000" << endl;
-       cout << " 2. Tiket Anak-anak  : Rp. 20.000" << endl;
-       cout << " =============================================" << endl;
-       cout << " Input Pembelian Tiket" << endl;
-       cout << " Jenis Tiket (1-2)  : "; cin >> jenisTKT;
-       cout << " Jumlah Tiket   : "; cin >> jumlahTKT; cout << endl;
-       cout << " =============================================" << endl;
-       
-       
-     if (jenisTKT==1)
-     {
-      totalTKT=jumlahTKT*35000;
-      jenisTXT="Dewasa";
-     }
-     else if (jenisTKT==2)
-     {
-      totalTKT=jumlahTKT*20000;
-      jenisTXT="Anak-anak";
-     }
-     else
-     {
-      cout << " Angka Yang Anda Input Salah";
-      cout << endl;
-      goto mulai;
-     }
-       cout << " Jenis Tiket   : " << jenisTXT << endl;
-       cout << " Jumlah Tiket   : " << jumlahTKT << endl;
-       cout << " ---------------------------------------------" << endl;
-       cout << " Total Bayar   : Rp. " <<totalTKT << endl;
-       cout << " ==============================================" << endl;
-       cout << endl;
+            cout << " =============================================" << endl;
+            cout << "\t   Pembelian Tiket " << endl;
+            cout << " =============================================" << endl;
+            cout << " Keterangan" << endl;
+            cout << " 1. Tiket Dewasa  : Rp. 35.000" << endl;
+            cout << " 2. Tiket Anak-anak  : Rp. 20.000" << endl;
+            cout << " =============================================" << endl;
 
-       break;
-       
-       
-     case 2:
+            cout << " Jenis Tiket (1-2)  : ";
+            cin >> jenisTKT;
+            cout << " Jumlah Tiket   : ";
+            cin >> jumlahTKT;
 
-      int KursiTerisi[2],kursiKSONG[2];
-       cout << " ==============================================" << endl;
-       cout << "\t   Cek Sisa Kursi "; cout << endl;
-       cout << " ==============================================" << endl;
-       cout << " Keterangan" << endl;
-       cout << " Jumlah Kursi Di Bioskop SerbaBebas sebangak 50 Kursi" << endl;
-       cout << " ==============================================" << endl;
-       cout << " Input Jumlah Kursi Yang Ingin Dipesan" << endl;
-       
-       angka=0;
+            if (jumlahTKT > totalKursi) {
+                cout << " Jumlah tiket yang diminta melebihi sisa kursi!" << endl;
+                break;
+            }
 
-      do
-     {
-      cout << " Jumlah Pemesanan Kursi  : "; cin >> KursiTerisi[angka];
-       angka++;
-     }
-     while (angka<1);
-      for (angka=0;angka<1;angka++)
-       kursiKSONG[angka]=50-KursiTerisi[angka];      
-	   cout << " ==============================================" << endl;
-       for (angka=0;angka<1;angka++)
-      {
-       cout << " Sisa Kursi   : "<< kursiKSONG[angka]; cout << endl;
-       cout << " ==============================================" << endl;
-       cout << endl;
-      }
-      
-        break;
+            if (jenisTKT == 1) {
+                totalTKT = jumlahTKT * 35000;
+                jenisTXT = "Dewasa";
+            } else if (jenisTKT == 2) {
+                totalTKT = jumlahTKT * 20000;
+                jenisTXT = "Anak-anak";
+            } else {
+                cout << " Jenis tiket tidak valid!" << endl;
+                break;
+            }
 
-   }
+            // Mengurangi jumlah kursi
+            totalKursi -= jumlahTKT;
 
- }
- while (pilihan!=3);
- }       
- 
- int main ()
- {
- 	pembelian_tiket_bioskop();
- 	return 0;
- }
+            cout << " =============================================" << endl;
+            cout << " Jenis Tiket   : " << jenisTXT << endl;
+            cout << " Jumlah Tiket  : " << jumlahTKT << endl;
+            cout << " ---------------------------------------------" << endl;
+            cout << " Total Bayar   : Rp. " << totalTKT << endl;
+            cout << " ==============================================" << endl;
+            cout << endl;
+
+            break;
+        }
+        case 2:
+            // Cek sisa kursi
+            cout << " ==============================================" << endl;
+            cout << "\t   Cek Sisa Kursi " << endl;
+            cout << " ==============================================" << endl;
+            cout << " Sisa Kursi di Bioskop: " << totalKursi << endl;
+            cout << " ==============================================" << endl;
+            cout << endl;
+            break;
+
+        case 3:
+            cout << " Terima kasih telah menggunakan layanan kami!" << endl;
+            break;
+
+        default:
+            cout << " Pilihan tidak valid. Silakan coba lagi." << endl;
+        }
+
+    } while (pilihan != 3);
+}
+
+int main() {
+    pembelian_tiket_bioskop();
+    return 0;
+}
